2724bbbf5749e528b1a6fa916d70db6f032f9e04
DucHoang-UIT <hoang.pham@executionlab.asia>
2024-03-01 08:25:32 +0700

    chore

--- Dockerfile
+++ Dockerfile
@@ -18,3 +18,20 @@ FROM builder AS development
 WORKDIR /server/app
 ENV ENVIRONMENT=development
 CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8080","--reload"]
+
+
+# FROM python:3.11.3
+# ENV PYTHONUNBUFFERED True
+
+# RUN pip install --upgrade pip
+# COPY requirements.txt .
+# RUN pip install --no-cache-dir -r  requirements.txt
+
+# ENV APP_HOME /root
+# WORKDIR $APP_HOME
+# COPY /app $APP_HOME/app
+
+# WORKDIR $APP_HOME/app
+
+# EXPOSE 8080
+# CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8080"]
---
+++ app/routers/gemi.py
@@ -0,0 +1,54 @@
+import textwrap
+from typing import Annotated
+
+import google.generativeai as genai
+from data import models
+from fastapi import APIRouter, Depends, status
+from fastapi.responses import StreamingResponse
+from google.cloud import secretmanager
+from IPython.display import Markdown
+from utils import get_current_active_user, get_dp
+
+
+def to_markdown(text):
+    text = text.replace("•", "  *")
+    return Markdown(textwrap.indent(text, "> ", predicate=lambda _: True))
+
+
+project_id = "forward-deck-415709"
+secret_id = "GOOGLE_API_KEY_ID"
+parent = f"projects/{project_id}"
+client = secretmanager.SecretManagerServiceClient()
+name = "projects/187132938708/secrets/GOOGLE_API_KEY_ID/versions/1"
+response = client.access_secret_version(request={"name": name})
+payload = response.payload.data.decode("UTF-8")
+
+
+genai.configure(api_key=payload)
+gemini_pro_vision_model = genai.GenerativeModel("gemini-pro")
+
+
+router_tag = "Gemi"
+
+router = APIRouter(
+    prefix="/chat-with-gemi",
+    tags=[router_tag],
+    responses={status.HTTP_404_NOT_FOUND: {"description": "Not found"}},
+)
+
+
+async def sendQuestion(in_message: str):
+    response = gemini_pro_vision_model.generate_content(in_message, stream=True)
+    for chunk in response:
+        yield (chunk.text)
+        print(chunk.text)
+
+
+@router.get("/gemi", tags=[router_tag])
+async def sendMessage(
+    message: str, _: Annotated[models.User, Depends(get_current_active_user)]
+):
+    return StreamingResponse(
+        sendQuestion(in_message=message),
+        media_type="text/event-stream",
+    )
