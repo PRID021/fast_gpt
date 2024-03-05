c564c99a589950119a096b54145e6560283941be
hoangduc <hoangduc.uit.dev@gmail.com>
2024-03-05 23:47:57 +0700

    fix docker compose

--- Dockerfile
+++ Dockerfile
@@ -10,7 +10,7 @@ ENV APP_HOME /root
 WORKDIR $APP_HOME
 COPY /app $APP_HOME/app
 
-WORKDIR $APP_HOME/app
+WORKDIR $APP_HOME
 
 EXPOSE 8080
-# CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8080"]
+CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8080"]
--- app/data/database.py
+++ app/data/database.py
@@ -2,17 +2,21 @@ from sqlalchemy import create_engine
 
 from sqlalchemy.orm import Session, sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://myuser:mypassword@0.0.0.0:5432/mydb"
+SQLALCHEMY_DATABASE_URL = "postgresql://myuser:mypassword@postgres:5432/gptservice"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(
     # SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False} # for sqlite
-    SQLALCHEMY_DATABASE_URL
+    SQLALCHEMY_DATABASE_URL,
+    echo=True,
 )
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine,expire_on_commit=False)
-
+SessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, bind=engine, expire_on_commit=False
+)
 
 
 def get_session() -> Session:
-    session = sessionmaker(autocommit=False, autoflush=False, bind=engine,expire_on_commit=False)
+    session = sessionmaker(
+        autocommit=False, autoflush=False, bind=engine, expire_on_commit=False
+    )
     return session()
